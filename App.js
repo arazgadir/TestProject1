c5047585f9cbdb1ed16101db5924dc15ff3ed739
Araz Gadirov <araz.gadirov@gamil.com>
2022-04-16 16:38:42 +0400

    ok

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+import { RootNavigation } from './source/navigation/RootNavigation'
+
+
+export const App = () => {
+  return (
+    <View>
+      <Text>
+        hello world
+      </Text>
+    </View>
+  )
+};    
